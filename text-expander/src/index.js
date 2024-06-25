e47d83a349bf320120c680078cc3093c71f13c52
anuragK2048 <developeranurag111@gmail.com>
2024-06-25 23:07:41 +0530

    new commit

---
+++ text-expander/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import {StarRating} from './StarRating';
+import App from "./tempChallenge"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>
+);
+
+
