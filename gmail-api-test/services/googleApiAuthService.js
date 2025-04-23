1947cbdc9c74798a1075e6d8dd1729814fcfb789
anuragK2048 <developeranurag111@gmail.com>
2025-04-23 23:59:32 +0530

    feat: defined get credential function

---
+++ gmail-api-test/services/googleApiAuthService.js
@@ -0,0 +1,44 @@
+const fs = require("fs").promises;
+const path = require("path");
+const process = require("process");
+const { authenticate } = require("@google-cloud/local-auth");
+const { google } = require("googleapis");
+
+//Define scopes
+const SCOPES = [
+  "https://www.googleapis.com/auth/gmail.readonly",
+  "https://www.googleapis.com/auth/gmail.send",
+];
+
+//Fetch and store token from files
+const TOKEN_PATH = path.join(process.cwd(), "./credentials/token.json");
+const CREDENTIALS_PATH = path.join(
+  process.cwd(),
+  "./credentials/credentials.json"
+);
+
+//Read previously authorized credentials from saved file
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+//Save credentials to token file
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.web || keys.installed;
+  //   const token = await client.getAccessToken();
+  const payload = {
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  };
+  await fs.writeFile(TOKEN_PATH, payload);
+}
